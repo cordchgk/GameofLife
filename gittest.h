60e2a80f9e9b47ed1587b84f4782e38cbf053881
cordchgk <cord-christian.goeken@iubh-fernstudium.de>
2021-03-01 18:51:34 +0100

    asd

--- gittest.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by cordch on 01.03.21.
-//
-
-#ifndef UNTITLED_GITTEST_H
-#define UNTITLED_GITTEST_H
-
-#endif //UNTITLED_GITTEST_H
---
+++ gittesttwo.h
@@ -0,0 +1,8 @@
+//
+// Created by cordch on 01.03.21.
+//
+
+#ifndef UNTITLED_GITTESTTWO_H
+#define UNTITLED_GITTESTTWO_H
+
+#endif //UNTITLED_GITTESTTWO_H
